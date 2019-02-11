b880c9a5c394a508f4afea6f682fc48ff1c640f1
Raine Wallace <rainebeaudubs@gmail.com>
2019-02-11 13:30:30 -0500

    Axios get request set up.

--- refugee-stories/src/App.js
+++ refugee-stories/src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stories: []
+    }
+  }
 
+  componentDidMount = () => {
+    axios
+      .get('https://refugeestories.herokuapp.com/api/stories')
+      .then(response => {
+        this.setState({
+          stories: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  };
 
   render() {
     return (
